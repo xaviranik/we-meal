cb0be7485340a9c5afb586a8b4e6c49b93788b94
Zabir Anik <xaviranik.me@gmail.com>
2022-02-13 19:20:20 +0600

    add: price meta box

--- src/Admin/CPT/Meal.php
+++ src/Admin/CPT/Meal.php
@@ -13,6 +13,8 @@ class Meal extends CustomPostType {
 	 */
 	public function register_hooks(): void {
 		add_filter( 'enter_title_here', [ $this, 'custom_enter_title' ] );
+		add_action( 'add_meta_boxes', [ $this, 'add_meta_box' ] );
+		add_action( 'save_post_meal', [ $this, 'save_meta_box' ] );
 	}
 
 	/**
@@ -44,7 +46,6 @@ class Meal extends CustomPostType {
 			'title',
 			'editor',
 			'thumbnail',
-			'revisions',
 		];
 
 		/*
@@ -66,7 +67,7 @@ class Meal extends CustomPostType {
 			'can_export'          => true,  // Allows content export using Tools -> Export
 			'has_archive'         => false,  // Enables post type archive (by month, date, or year)
 			'exclude_from_search' => false, // Exclude posts of this type in the front-end search result page if set to true, include them if set to false
-			'publicly_queryable'  => true,  // Allow queries to be performed on the front-end part if set to true
+			'publicly_queryable'  => false,  // Allow queries to be performed on the front-end part if set to true
 			'capability_type'     => 'post', // Allows read, edit, delete like “Post”
 		];
 
@@ -81,9 +82,86 @@ class Meal extends CustomPostType {
 	 * @return string
 	 */
 	public function custom_enter_title( $title ): string {
-		if ( get_post_type() === 'meal' ) {
+		if ( 'meal' === get_post_type() ) {
 			$title = __( 'Enter the name of the meal', 'we-meal' );
 		}
 		return $title;
+	}
+
+	/**
+	 * Adds the meta box container.
+	 *
+	 * @param $post_type
+	 *
+	 * @return void
+	 */
+	public function add_meta_box( $post_type ): void {
+		$post_types = [ 'meal' ];
+
+		if ( in_array( $post_type, $post_types, true ) ) {
+			add_meta_box(
+				'we-meal-price-meta-box',
+				__( 'Meal Price', 'we-meal' ),
+				[ $this, 'render_meta_box_content' ],
+				$post_type,
+				'side',
+				'high'
+			);
+		}
+	}
+
+
+	/**
+	 * Render Meta Box content.
+	 *
+	 * @param \WP_Post $post The post object.
+	 *
+	 * @return void
+	 */
+	public function render_meta_box_content( \WP_Post $post ): void {
+		wp_nonce_field( 'we_meal_price_meta_box', 'we_meal_price_meta_box_nonce' );
+
+		$price = get_post_meta( $post->ID, '_we_meal_price', true );
+
+		?>
+		<label for="we_meal_price_field">
+			<input placeholder="Meal price" name="we_meal_price" type="number" step="0.01" value="<?php echo esc_attr( $price ); ?>" class="form-control" style="width: 100%;" >
+		</label>
+		<?php
+	}
+
+	/**
+	 * Save the meta when the post is saved.
+	 *
+	 * @param $post_id
+	 *
+	 * @return void
+	 */
+	public function save_meta_box( $post_id ): void {
+		if ( ! isset( $_POST['we_meal_price_meta_box_nonce'] ) ) {
+			return;
+		}
+
+		if ( ! current_user_can( 'manage_options', $post_id ) ) {
+			return;
+		}
+
+		$nonce = sanitize_key( wp_unslash( $_POST['we_meal_price_meta_box_nonce'] ) );
+
+		if ( ! wp_verify_nonce( $nonce, 'we_meal_price_meta_box' ) ) {
+			return;
+		}
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		if ( ! isset( $_POST['we_meal_price'] ) ) {
+			return;
+		}
+
+		$price = sanitize_text_field( wp_unslash( $_POST['we_meal_price'] ) );
+
+		update_post_meta( $post_id, '_we_meal_price', $price );
 	}
 }
