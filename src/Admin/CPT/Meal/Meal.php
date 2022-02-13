be993665c7733b728ab2714aa8ce9e294a0f2008
Zabir Anik <xaviranik.me@gmail.com>
2022-02-14 00:19:20 +0600

    add: DI meta box for price

--- src/Admin/CPT/Meal/Meal.php
+++ src/Admin/CPT/Meal/Meal.php
@@ -3,7 +3,6 @@
 namespace PhpKnight\WeMeal\Admin\CPT\Meal;
 
 use PhpKnight\WeMeal\Core\Abstracts\CustomPostType;
-use WP_Post;
 
 /**
  * Class Meal.
@@ -13,14 +12,22 @@ use WP_Post;
 class Meal extends CustomPostType {
 
 	/**
+	 * Meal CPT constructor.
+	 *
+	 * @var string
+	 */
+	public function __construct( PriceMetaBox $price_meta_box ) {
+		parent::__construct();
+		$price_meta_box->register_hooks();
+	}
+
+	/**
 	 * Registers all hooks for the class.
 	 *
 	 * @return void
 	 */
 	public function register_hooks(): void {
 		add_filter( 'enter_title_here', [ $this, 'custom_enter_title' ] );
-		add_action( 'add_meta_boxes', [ $this, 'add_meta_box' ] );
-		add_action( 'save_post_meal', [ $this, 'save_meta_box' ] );
 	}
 
 	/**
@@ -100,87 +107,6 @@ class Meal extends CustomPostType {
 			$title = __( 'Enter the name of the meal', 'we-meal' );
 		}
 		return $title;
-	}
-
-	/**
-	 * Adds the meta box container.
-	 *
-	 * @param $post_type
-	 *
-	 * @return void
-	 */
-	public function add_meta_box( $post_type ): void {
-		$post_types = [ 'meal' ];
-
-		if ( in_array( $post_type, $post_types, true ) ) {
-			add_meta_box(
-				'we-meal-price-meta-box',
-				__( 'Meal Price', 'we-meal' ),
-				[ $this, 'render_meta_box_content' ],
-				$post_type,
-				'side',
-				'high'
-			);
-		}
-	}
-
-
-	/**
-	 * Render Meta Box content.
-	 *
-	 * @param WP_Post $post The post object.
-	 *
-	 * @return void
-	 */
-	public function render_meta_box_content( WP_Post $post ): void {
-		wp_nonce_field( 'we_meal_price_meta_box', 'we_meal_price_meta_box_nonce' );
-
-		$price = get_post_meta( $post->ID, '_we_meal_price', true );
-
-		?>
-		<label for="we_meal_price_field">
-			<input placeholder="Meal price" name="we_meal_price" type="number" step="0.01" value="<?php echo esc_attr( $price ); ?>" class="form-control" style="width: 100%;" >
-		</label>
-		<?php
-	}
-
-	/**
-	 * Save the meta when the post is saved.
-	 *
-	 * @param $post_id
-	 *
-	 * @return void
-	 */
-	public function save_meta_box( $post_id ): void {
-		if ( ! isset( $_POST['we_meal_price_meta_box_nonce'] ) ) {
-			return;
-		}
-
-		if ( ! current_user_can( 'manage_options', $post_id ) ) {
-			return;
-		}
-
-		$nonce = sanitize_key( wp_unslash( $_POST['we_meal_price_meta_box_nonce'] ) );
-
-		if ( ! wp_verify_nonce( $nonce, 'we_meal_price_meta_box' ) ) {
-			return;
-		}
-
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-			return;
-		}
-
-		if ( ! isset( $_POST['we_meal_price'] ) ) {
-			return;
-		}
-
-		$price = sanitize_text_field( wp_unslash( $_POST['we_meal_price'] ) );
-
-		if ( ! empty( $price ) ) {
-			update_post_meta( $post_id, '_we_meal_price', $price );
-		} else {
-			delete_post_meta( $post_id, '_we_meal_price' );
-		}
 	}
 }
 
---
+++ src/Core/Abstracts/MetaBox.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PhpKnight\WeMeal\Core\Abstracts;
+
+use PhpKnight\WeMeal\Core\Interfaces\HookableInterface;
+
+/**
+ * Class MetaBox
+ *
+ * @package PhpKnight\WeMeal\Core\Abstracts
+ */
+abstract class MetaBox implements HookableInterface {
+
+}
