f2916511d06debecd1899cc2bf5fe8cb00d39ca9
Zabir Anik <xaviranik.me@gmail.com>
2022-02-20 19:59:31 +0600

    add: meal model

---
+++ src/Models/MealModel.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace PhpKnight\WeMeal\Models;
+
+use PhpKnight\WeMeal\Admin\CPT\Meal\PriceMetaBox;
+use PhpKnight\WeMeal\Helper;
+use WP_Post;
+
+class MealModel {
+
+	/**
+	 * @var WP_Post
+	 */
+	protected $meal;
+
+	/**
+	 * @var int
+	 */
+	protected $id;
+
+	/**
+	 * @var string
+	 */
+	protected $name;
+
+	/**
+	 * @var string
+	 */
+	protected $description;
+
+	/**
+	 * @var float
+	 */
+	protected $price;
+
+	/**
+	 * @var string
+	 */
+	protected $formatted_price;
+
+	/**
+	 * @var string
+	 */
+	protected $image;
+
+	/**
+	 * MealModel constructor.
+	 */
+	public function __construct( WP_Post $meal ) {
+		$this->meal = $meal;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function get_id(): int {
+		$this->id = $this->meal->ID;
+
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 *
+	 * @return MealModel
+	 */
+	public function set_id( int $id ): MealModel {
+		$this->id = $id;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_name(): string {
+		$this->name = $this->meal->post_name;
+
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return MealModel
+	 */
+	public function set_name( string $name ): MealModel {
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_description(): string {
+		$this->description = $this->meal->post_content;
+
+		return $this->description;
+	}
+
+	/**
+	 * @param string $description
+	 *
+	 * @return MealModel
+	 */
+	public function set_description( string $description ): MealModel {
+		$this->description = $description;
+
+		return $this;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function get_price(): float {
+		$this->price = floatval( get_post_meta( $this->meal->ID, PriceMetaBox::$price_meta_key, true ) );
+
+		return $this->price;
+	}
+
+	/**
+	 * @param float $price
+	 *
+	 * @return MealModel
+	 */
+	public function set_price( float $price ): MealModel {
+		$this->price = $price;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_formatted_price(): string {
+		$this->formatted_price = Helper::format_price( $this->get_price() );
+
+		return $this->formatted_price;
+	}
+
+	/**
+	 * @param string $formatted_price
+	 *
+	 * @return MealModel
+	 */
+	public function set_formatted_price( string $formatted_price ): MealModel {
+		$this->formatted_price = $formatted_price;
+
+		return $this;
+	}
+
+	/**
+	 * @param string $size
+	 *
+	 * @return string
+	 */
+	public function get_image( string $size = 'thumbnail' ): string {
+		$this->image = get_the_post_thumbnail_url( $this->meal->ID, $size );
+
+		return $this->image;
+	}
+
+	/**
+	 * @param string $image
+	 *
+	 * @return MealModel
+	 */
+	public function set_image( string $image ): MealModel {
+		$this->image = $image;
+
+		return $this;
+	}
+
+}
