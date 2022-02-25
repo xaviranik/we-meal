9d45a3ea26c39afb36fa2e7a9e298191ec277bf0
Zabir Anik <xaviranik.me@gmail.com>
2022-02-25 09:58:19 +0600

    add: modal popper style

--- assets/src/components/ModalPopper.js
+++ assets/src/components/ModalPopper.js
@@ -4,7 +4,7 @@ const ModalPopper = ({openModal, closeModal, title, content}) => {
 	return (
 		<Modal open={openModal} onClose={closeModal} classNames={{modal: 'wm-rounded-md wm-w-80 md:wm-w-256'}} center>
 			<h1 className={'wm-text-xl wm-font-semibold'}>{title}</h1>
-			<hr className={'wm-mt-4'}/>
+			<hr className={'wm-mt-4 -wm-mx-4'}/>
 			<div>
 				{content}
 			</div>
