33db467f2c747b8176505f2d40b2ea9a091ad69c
Zabir Anik <xaviranik.me@gmail.com>
2022-02-27 18:01:20 +0600

    add: meal calendar

--- assets/src/components/MealCalendar.js
+++ assets/src/components/MealCalendar.js
@@ -1,9 +1,22 @@
 import { __ } from '@wordpress/i18n';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 const MealCalendar = () => {
 	return (
 		<div className={'wm-card'}>
 			<h1 className={'wm-text-base wm-font-semibold'}>{__( 'Meal Calendar', 'we-meal' )}</h1>
+
+			<div className={'wm-mt-4'}>
+				<FullCalendar
+					plugins={[ dayGridPlugin ]}
+					initialView="dayGridMonth"
+					events={[
+						{ title: 'Rice Chicken', date: '2022-02-11' },
+						{ title: 'Rice Beef', date: '2022-02-14' }
+					]}
+				/>
+			</div>
 		</div>
 	);
 };
--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b7792cdb688861a00a2c21518af13810');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '00d53a1fcbdef126a23c912021456fb5');
