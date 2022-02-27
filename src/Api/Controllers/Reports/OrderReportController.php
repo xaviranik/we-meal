5c2636bcf359c85b18d4774a107c88349c789c90
Zabir Anik <xaviranik.me@gmail.com>
2022-02-28 00:15:31 +0600

    add: order report model

--- src/Api/Controllers/Reports/OrderReportController.php
+++ src/Api/Controllers/Reports/OrderReportController.php
@@ -2,6 +2,7 @@
 
 namespace PhpKnight\WeMeal\Api\Controllers\Reports;
 
+use PhpKnight\WeMeal\Models\OrderReportModel;
 use WP_REST_Controller;
 use PhpKnight\WeMeal\Api\Api;
 use WP_REST_Request;
@@ -10,13 +11,20 @@ use WP_REST_Server;
 class OrderReportController extends WP_REST_Controller {
 
 	/**
+	 * @var OrderReportModel
+	 */
+	protected $order_report_model;
+
+	/**
 	 * Meal menu controller constructor.
 	 *
 	 * @return void
 	 */
-	public function __construct() {
+	public function __construct( OrderReportModel $order_report_model ) {
 		$this->namespace = Api::$namespace;
 		$this->rest_base = 'reports';
+
+		$this->order_report_model = $order_report_model;
 	}
 
 	/**
@@ -27,19 +35,30 @@ class OrderReportController extends WP_REST_Controller {
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
-			'/' . $this->rest_base . '/user/(?P<id>[\d]+)/order',
+			'/' . $this->rest_base . '/user/meal-calendar',
 			[
-				'args'   => [],
 				[
 					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => [ $this, 'get_user_order_report' ],
+					'callback'            => [ $this, 'get_meal_calendar_data' ],
 					'permission_callback' => [ $this, 'get_order_report_permissions_check' ],
 					'args'                => [
-						'id'    => [
+						'user_id' => [
 							'description'       => __( 'Unique identifier for the user.', 'we-meal' ),
-							'required'          => true,
+							'required'          => false,
 							'type'              => 'integer',
 							'sanitize_callback' => 'absint',
+						],
+						'start_date' => [
+							'description'       => __( 'Start date for the report.', 'we-meal' ),
+							'required'          => true,
+							'type'              => 'string',
+							'sanitize_callback' => 'sanitize_text_field',
+						],
+						'end_date' => [
+							'description'       => __( 'End date for the report.', 'we-meal' ),
+							'required'          => true,
+							'type'              => 'string',
+							'sanitize_callback' => 'sanitize_text_field',
 						],
 					],
 				],
@@ -56,7 +75,19 @@ class OrderReportController extends WP_REST_Controller {
 	 * @return bool
 	 */
 	public function get_order_report_permissions_check( WP_REST_Request $request ): bool {
-		return is_user_logged_in() && current_user_can( 'manage_meal' );
+		if ( ! is_user_logged_in() ) {
+			return false;
+		}
+
+		if ( current_user_can( 'manage_meal' ) ) {
+			return true;
+		}
+
+		if ( get_current_user_id() !== (int) $request->get_param( 'id' ) ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
@@ -67,6 +98,23 @@ class OrderReportController extends WP_REST_Controller {
 	 * @return \WP_REST_Response
 	 */
 	public function get_user_order_report( WP_REST_Request $request ): \WP_REST_Response {
-		return rest_ensure_response( $request->get_params() );
+		return rest_ensure_response( $request->get_param( 'id' ) );
+	}
+
+
+	/**
+	 * Gets meal calendar data.
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return \WP_Error|\WP_HTTP_Response|\WP_REST_Response
+	 */
+	public function get_meal_calendar_data( WP_REST_Request $request ) {
+		$user_id = $request->get_param( 'user_id' ) ?? get_current_user_id();
+
+		$calendar_data = $this->order_report_model
+						->get_meal_calendar_data_by_user( $user_id, $request->get_param( 'start_date' ), $request->get_param( 'end_date' ) );
+
+		return rest_ensure_response( $calendar_data );
 	}
 }
--- src/Models/OrderReportModel.php
+++ src/Models/OrderReportModel.php
@@ -4,5 +4,44 @@ namespace PhpKnight\WeMeal\Models;
 
 class OrderReportModel {
 
+	/**
+	 * Gets the meal calendar data.
+	 *
+	 * @param int $user_id
+	 * @param string $start_date
+	 * @param string $end_date
+	 *
+	 * @return array
+	 */
+	public function get_meal_calendar_data_by_user( int $user_id, string $start_date, string $end_date ): array {
+		return $this->get_order_data_by_user( $user_id, $start_date, $end_date );
+	}
 
+	/**
+	 * Gets the order report data.
+	 *
+	 * @param int $user_id
+	 * @param string $start_date
+	 * @param string $end_date
+	 *
+	 * @return array
+	 */
+	public function get_order_data_by_user( int $user_id, string $start_date, string $end_date ): array {
+		global $wpdb;
+
+		$start_date = current_datetime()->modify( $start_date )->format( 'Y-m-d' );
+		$end_date   = current_datetime()->modify( $end_date )->format( 'Y-m-d' );
+
+		return $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT *
+				FROM
+					{$wpdb->prefix}we_meal_orders
+				WHERE
+					user_id = %d
+					AND created_at BETWEEN %s AND %s;",
+				$user_id, $start_date, $end_date
+			)
+		);
+	}
 }
